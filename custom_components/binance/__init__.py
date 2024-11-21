2ebb30f517e2e86a21dd6cbe83823c5e18743b6a
cataseven <52464808+cataseven@users.noreply.github.com>
2024-11-21 17:49:25 +0300

    Update __init__.py

--- custom_components/binance/__init__.py
+++ custom_components/binance/__init__.py
@@ -42,4 +42,5 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Yapılandırma girişi güncellendiğinde işleyin."""
-    await hass.config_entries.async_reload(entry.entry_id)
+    await hass.config_entries.async_unload(entry.entry_id)
+    await hass.config_entries.async_setup(entry.entry_id)
