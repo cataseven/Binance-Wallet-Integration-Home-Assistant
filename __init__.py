114a2561d6e151a26cb9125822491453656da36f
cataseven <52464808+cataseven@users.noreply.github.com>
2024-11-21 15:40:04 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,45 @@
+import asyncio
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, PLATFORMS
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Entegrasyonu kurun."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Yapılandırma girişini kurun."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Platformları asenkron olarak kurun.
+    await asyncio.gather(
+        *[
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+            for platform in PLATFORMS
+        ]
+    )
+
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Yapılandırma girişini kaldırın."""
+    unloaded = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, platform)
+                for platform in PLATFORMS
+            ]
+        )
+    )
+    if unloaded:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unloaded
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Yapılandırma girişi güncellendiğinde işleyin."""
+    await hass.config_entries.async_reload(entry.entry_id)
